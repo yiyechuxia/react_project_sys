a197d875deec0a776db98c32c4a001da09327eca
qinhui <qinhui@timesdata.cn>
2021-11-06 18:27:23 +0800

    整理-菜单数据化

--- src/containers/Sidebar/index.jsx
+++ src/containers/Sidebar/index.jsx
@@ -12,44 +12,70 @@ import {
 } from "@ant-design/icons"
 const {SubMenu} = Menu
 const { Sider } = Layout
+const menuList = [
+  {id:1,icon:'UserOutlined',title:'用户管理',key:'1-1',route:'/management/user'},
+  {id:2,icon:'VideoCameraOutlined',title:'角色管理',key:'1-2',route:'/management/role'},
+  {id:3,icon:'TeamOutlined',title:'菜单管理',key:'1-3',route:'/management/menu'},
+  {id:4,icon:'UploadOutlined',title:'部门管理',key:'1-4',route:'/management/department'},
+  {id:5,icon:'DingtalkOutlined',title:'岗位管理',key:'1-5',route:'/management/jobs'},
+  {id:6,icon:'DingtalkOutlined',title:'字典管理',key:'1-6',route:'/management/dictionary'},
+  {id:7,icon:'DingtalkOutlined',title:'参数管理',key:'1-7',route:'/management/params'},
+  {id:8,icon:'DingtalkOutlined',title:'通知公告',key:'1-8',route:'/management/notice'},
+  {id:9,icon:'AppstoreOutlined',title:'日志管理',key:'1-9',route:null , childern:[
+    {id:10,title:'操作日志',key:'1-9-1',route:'/management/log/operation'},
+    {id:11,title:'登录日志',key:'1-9-2',route:'/management/log/loginlog'},
+  ]},
+]
 class Sidebar extends Component {
   render() {
     return (
       // 侧边栏菜单
       <Sider trigger={null} collapsible collapsed={this.props.switchV}>
       <div className="logo" />
-      <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-        <Menu.Item key="1" icon={<UserOutlined />} onClick={()=>{this.props.history.push('/management/user')}}>
-          用户管理
-        </Menu.Item>
-        <Menu.Item key="2" icon={<VideoCameraOutlined />} onClick={()=>{this.props.history.push('/management/role')}}>
-          角色管理
-        </Menu.Item>
-        <Menu.Item key="3" icon={<TeamOutlined />} onClick={()=>{this.props.history.push('/management/menu')}}>
-          菜单管理
-        </Menu.Item>
-        <Menu.Item key="5" icon={<UploadOutlined />} onClick={()=>{this.props.history.push('/management/department')}} >
-          部门管理
-        </Menu.Item>
-        <Menu.Item key="6" icon={<DingtalkOutlined />} onClick={()=>{this.props.history.push('/management/jobs')}}>
-          岗位管理 
-        </Menu.Item>
-        <Menu.Item key="7" icon={<DingtalkOutlined />} onClick={()=>{this.props.history.push('/management/dictionary')}}>
-          字典管理
-        </Menu.Item>
-        <Menu.Item key="8" icon={<DingtalkOutlined />} onClick={()=>{this.props.history.push('/management/params')}}>
-          参数设置
-        </Menu.Item>
-        <Menu.Item key="9" icon={<DingtalkOutlined />} onClick={()=>{this.props.history.push('/management/notice')}}>
-          通知公告
-        </Menu.Item>
-        {/* <Menu.Item key="10" icon={<DingtalkOutlined />}>
-          日志管理
-        </Menu.Item> */}
-        <SubMenu key="10" icon={<AppstoreOutlined />} title="通知公告">
-            <Menu.Item key="10-1" onClick={()=>{this.props.history.push('/management/log/operation')}}>操作日志</Menu.Item>
-            <Menu.Item key="10-2" onClick={()=>{this.props.history.push('/management/log/loginlog')}}>登录日志</Menu.Item>
-        </SubMenu>
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={["1-1"]}>
+        {menuList.map(item=>{
+          if(item.childern){
+           return <SubMenu key={item.key} icon={item.icon} title={item.title}>
+                      {item.childern.map(el=>{
+                        return <Menu.Item key={el.key} onClick={()=>{this.props.history.push(el.route)}}>{el.title}</Menu.Item>
+                      })}
+                  </SubMenu>
+          }else{
+            return (
+              <Menu.Item key={item.key} icon={(<UserOutlined />)} onClick={()=>{this.props.history.push(item.route)}}>
+                {item.title}
+              </Menu.Item>
+            )
+          }
+        })}
+          {/* <Menu.Item key="1-1" icon={<UserOutlined />} onClick={()=>{this.props.history.push('/management/user')}}>
+            用户管理
+          </Menu.Item>
+          <Menu.Item key="1-2" icon={<VideoCameraOutlined />} onClick={()=>{this.props.history.push('/management/role')}}>
+            角色管理
+          </Menu.Item>
+          <Menu.Item key="1-3" icon={<TeamOutlined />} onClick={()=>{this.props.history.push('/management/menu')}}>
+            菜单管理
+          </Menu.Item>
+          <Menu.Item key="1-4" icon={<UploadOutlined />} onClick={()=>{this.props.history.push('/management/department')}} >
+            部门管理
+          </Menu.Item>
+          <Menu.Item key="1-5" icon={<DingtalkOutlined />} onClick={()=>{this.props.history.push('/management/jobs')}}>
+            岗位管理 
+          </Menu.Item>
+          <Menu.Item key="1-6" icon={<DingtalkOutlined />} onClick={()=>{this.props.history.push('/management/dictionary')}}>
+            字典管理
+          </Menu.Item>
+          <Menu.Item key="1-7" icon={<DingtalkOutlined />} onClick={()=>{this.props.history.push('/management/params')}}>
+            参数设置
+          </Menu.Item>
+          <Menu.Item key="1-8" icon={<DingtalkOutlined />} onClick={()=>{this.props.history.push('/management/notice')}}>
+            通知公告
+          </Menu.Item>
+          <SubMenu key="1-9" icon={<AppstoreOutlined />} title="日志管理">
+              <Menu.Item key="1-9-1" onClick={()=>{this.props.history.push('/management/log/operation')}}>操作日志</Menu.Item>
+              <Menu.Item key="1-9-2" onClick={()=>{this.props.history.push('/management/log/loginlog')}}>登录日志</Menu.Item>
+          </SubMenu> */}
       </Menu>
     </Sider>
     )
